06d413a6fa8dc011ec6bbe0d7de57752978adadf
Volodymyr Kolchin <vkolchin@codifi.me>
2022-02-07 12:07:55 +0200

    comment remove

--- pages/import-products/index.tsx
+++ pages/import-products/index.tsx
@@ -1,29 +1,19 @@
-import { Button, Checkbox, Flex, FormGroup, Input, Panel, Select, Form as StyledForm, Textarea } from '@bigcommerce/big-design';
+import { Button, Flex, FormGroup, Input, Panel, Form as StyledForm } from '@bigcommerce/big-design';
 
-import { useRouter } from 'next/router';
-import {ReactElement, useState, useEffect, useMemo, ChangeEvent} from 'react';
+import {useState, ChangeEvent} from 'react';
 import ErrorMessage from '../../components/error';
 import Loading from '../../components/loading';
 import {useProductListAll} from '../../lib/hooks';
 import { CSVLink } from "react-csv";
-// import Crontab from 'reactjs-crontab';
-// import 'reactjs-crontab/dist/index.css';
-// import { SMTPClient } from 'emailjs';
+
 import emailjs from '@emailjs/browser';
-import {FormData} from "@types";
+
 
 interface FormProps {
     formData: FormData;
-    onCancel(): void;
-    onSubmit(form: FormData): void;
 }
 
-const importProducts = ({ formData, onCancel, onSubmit }: FormProps) => {
-    // Get the router object
-    // const router = useRouter();
-    // const [email, setEmail] = useState('');
-    // const { email } = formData;
-    // const { description, type, email}= formData;
+const importProducts = ({ formData}: FormProps) => {
     const [form, setForm] = useState({ email: '' });
     const dataImportProduct = [];
     const { error, isLoading, list = [], meta = {}, mutateList=[] } = useProductListAll();
@@ -38,13 +28,6 @@ const importProducts = ({ formData, onCancel, onSubmit }: FormProps) => {
     if (isLoading) return <Loading />;
     if (error) return <ErrorMessage error={error} />;
 
-    // const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    //     // const { name: formName, value } = event?.target;
-    //     // setForm(prevForm => ({ ...prevForm, [formName]: value }));
-    //     console.log('event?.target', event?.target);
-    //     console.log('event', event);
-    //     setEmail(event?.target);
-    // };
     const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name: formName, value } = event?.target;
         setForm(prevForm => ({ ...prevForm, [formName]: value }));
