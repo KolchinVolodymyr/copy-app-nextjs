2eee9773d21c6e9bb96ff29cc409ba2ac3f99bd7
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-02-12 13:39:17 +0200

    commit

--- scripts/bcSdk.js
+++ scripts/bcSdk.js
@@ -3,36 +3,36 @@ import $ from 'jquery';
 
 export function bigCommerceSDK(context) {
 
-        console.log('111111111111111111');
+        // console.log('111111111111111111');
         // console.log('cron', cron);
     if (typeof window === "undefined") return;
-    console.log('222222222222222');
-    console.log('window', window)
+    // console.log('222222222222222');
+    // console.log('window', window)
     const s = 'script';
     const id = 'bigcommerce-sdk-js';
     const d = document;
     let js, bcjs = d.getElementsByTagName(s)[0];
-    console.log('d', d);
+    // console.log('d', d);
     if (d.getElementById(id)) return;
-    console.log('$$$',$);
+    // console.log('$$$',$);
 
     js = d.createElement(s);
-    console.log('js', js);
+    // console.log('js', js);
 
     js.id = id;
     js.async = true;
     js.src = "https://cdn.bigcommerce.com/jssdk/bc-sdk.js";
     bcjs.parentNode.insertBefore(js, bcjs);
 
-    $(document).ready(function() {
-        // $('#selector')
-        console.log('Hi !');
-        console.log('$(h2)', $('h2'));
-        console.log('my-custom-id', $('#my-custom-id'))
-        console.log('my-custom-id-home', $('#my-custom-id-home'))
-    });
-    console.log('my-custom-id', $('#my-custom-id'))
-    console.log('my-custom-id-home', $('#my-custom-id-home'))
+    // $(document).ready(function() {
+    //     // $('#selector')
+    //     console.log('Hi !');
+    //     console.log('$(h2)', $('h2'));
+    //     console.log('my-custom-id', $('#my-custom-id'))
+    //     console.log('my-custom-id-home', $('#my-custom-id-home'))
+    // });
+    // console.log('my-custom-id', $('#my-custom-id'))
+    // console.log('my-custom-id-home', $('#my-custom-id-home'))
 
     window.bcAsyncInit = function() {
         Bigcommerce.init({
