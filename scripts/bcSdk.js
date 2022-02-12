eec2a8cff3e601330f2e421e9f91ba13f5785454
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-02-12 11:38:29 +0200

    commit

--- scripts/bcSdk.js
+++ scripts/bcSdk.js
@@ -7,10 +7,11 @@ export function bigCommerceSDK(context) {
     const id = 'bigcommerce-sdk-js';
     const d = document;
     let js, bcjs = d.getElementsByTagName(s)[0];
-
+    console.log('d', d);
     if (d.getElementById(id)) return;
 
     js = d.createElement(s);
+    console.log('js', js);
     js.id = id;
     js.async = true;
     js.src = "https://cdn.bigcommerce.com/jssdk/bc-sdk.js";
